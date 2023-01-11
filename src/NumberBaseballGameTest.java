7e7507e6d7d5960d20d9a6945cea1ca812d5485c
Wooyoung Jung <wooyounj@amazon.com>
2023-01-11 21:55:56 +0900

    문제 조건 추가

--- src/NumberBaseballGameTest.java
+++ src/NumberBaseballGameTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertNotNull;
 public class NumberBaseballGameTest {
     @Test
     public void shouldNotNull() {
-        // TODO: 이 테스트는 의미가 없으니 지우셔도 됩니다.
         NumberBaseballGame game = new NumberBaseballGame();
         assertNotNull(game);
     }
